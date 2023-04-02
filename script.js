3a0dd3c0aea5d531d9a03187958a5bdfa11f6818
arazoko223 <marioajipraseyawan@gmail.com>
2023-04-02 19:54:34 +0700

    Catatan terkait perubahan yang anda buatupdate

---
+++ script.js
@@ -0,0 +1,9 @@
+    // typing text animation script
+    var typed = new Typed(".typing", {
+        strings: ["Follow","Share", "Komen", "Donate"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        cursorChar: ' |',
+        smartBackspace: true,
+        loop: true
+    });
