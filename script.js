775895f9cd31694abb053302ebebc77ba15ad439
arazoko223 <marioajipraseyawan@gmail.com>
2023-10-09 15:04:00 +0700

    Menambahkan proyek situs web saya

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Follow","Share", "Komen", "Donate"],
+        strings: ["SENIN - SABTU","11:00 - 13:00", "19:30 - 23:30"],
         typeSpeed: 100,
         backSpeed: 60,
         cursorChar: ' |',
         smartBackspace: true,
-        loop: true
+        loop: true ,
+        
     });
